0ea1b984b8928dcd39215a54ce213f2c97c53377
李逍遥 <3379489032@qq.com>
2021-07-16 16:42:28 +0800

    gitbook init

---
+++ book.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "title": "lixiaoyao",
+  "description": "lixiaoyao的前端小册子",
+  "author": "lixiaoyao",
+  "language": "zh-hans",
+  "links": {
+    "sidebar": {
+      
+    }
+  },
+  "styles": {
+    "website": "style.css"
+  },
+  "plugins": [
+    "expandable-chapters",
+    "search-plus",
+    "splitter",
+    "github",
+    "back-to-top-button"
+  ],
+  "pluginsConfig": {
+    "github": {
+      "url": "https://github.com/lidenghui/gitbook"
+    }
+  }
+}
