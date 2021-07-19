0e65edd47be3f136c6c68c9855b512150e7ba308
李逍遥 <3379489032@qq.com>
2021-07-19 16:58:02 +0800

    Updates

--- script/publish.js
+++ script/publish.js
@@ -1,5 +1,8 @@
 var ghpages = require('gh-pages');
+var execa = require('execa');
 
+execa.commandSync('gitbook install');
+execa.commandSync('gitbook build');
 ghpages.publish('_book', function(err) {
     console.log(err)
 });
